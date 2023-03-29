45d9e71de595dcdbbb34c5071f7f4edeb784663d
mateusvg <mateusvitorino@outlook.com.br>
2023-03-29 15:08:02 -0300

    report certificate status

--- src/pages/Admin/Reports.tsx
+++ src/pages/Admin/Reports.tsx
@@ -5,6 +5,8 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Box, Button } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { getAllRegistersUsersStatus } from '../../services/Admin/getAllRegistersUsersStatus'
 
 export default function ControlledAccordions() {
   const [expanded, setExpanded] = React.useState<string | false>(false);
@@ -14,9 +16,47 @@ export default function ControlledAccordions() {
       setExpanded(isExpanded ? panel : false);
     };
 
+
+
+  type personsType = {
+    idmedicalCertificate: string
+    patientName: string
+    attachment: any
+    patientCpf: string
+    fitness: string
+    status: string
+  }
+  const [persons, setPerson] = useState<personsType[] | []>([]);
+  useEffect(() => {
+    getAllHistoryRegisters()
+  }, [])
+
+  const getAllHistoryRegisters = async () => {
+    const data1 = await getAllRegistersUsersStatus()
+    setPerson(data1)
+  };
+
+
+
+  // Download file report XLS OR TXT
+  function downloadFile(e: any) {
+    const element = document.createElement("a")
+    let novoArray: any = ["ID", "Nome", "CPF", "Status", "Aptidão"]
+    let arraySemAnexo: any = []
+    persons?.map(function (item, indice) {
+      return arraySemAnexo.push(` \n ${persons[indice]['idmedicalCertificate']}, ${persons[indice]['patientName']} , ${persons[indice]['patientCpf']} , ${persons[indice]['status'] }, ${persons[indice]['fitness'] }`)
+    });
+
+    const file = new Blob([`${novoArray} \n ${arraySemAnexo}`]);
+    e.preventDefault()
+    element.href = URL.createObjectURL(file);
+    element.download = `Relatorio.xlsx`
+    element.click();
+
+  }
   return (
     <div>
-      < Box  display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} margin={3}>
+      < Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'} margin={3}>
         Relatórios
       </Box>
       <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
@@ -31,7 +71,7 @@ export default function ControlledAccordions() {
           <Typography sx={{ color: 'text.secondary' }}>Relatório de status de atestado médico</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Button>
+          <Button onClick={downloadFile}>
             Executar
           </Button>
         </AccordionDetails>
@@ -49,7 +89,7 @@ export default function ControlledAccordions() {
           <Typography sx={{ color: 'text.secondary' }}>Relatório de extrato de ponto</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Button>
+          <Button >
             Executar
           </Button>
         </AccordionDetails>
